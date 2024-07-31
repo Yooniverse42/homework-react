f257292ecba67f5355184fbb3c11ee3af8e5d058
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-08-01 08:50:53 +0900

    Vite 매뉴얼 스캐폴딩 과제(문제 해결 전)

---
+++ vite-custom-template/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+
+const domNode = document.getElementById('react-app');
+createRoot(domNode).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
