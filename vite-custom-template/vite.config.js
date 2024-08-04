e69c324a2209f238b2ba66899a496c32e9715f79
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-08-04 18:53:35 +0900

    기본세팅

--- vite-custom-template/vite.config.js
+++ vite-custom-template/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
-import pluginReact from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 const viteConfig = defineConfig({
-  plugins: [
-    pluginReact()
-  ]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
 
 export default viteConfig;
