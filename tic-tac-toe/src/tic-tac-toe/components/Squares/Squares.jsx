37ce67b4ec194b3c451d758288f235e998b50879
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2024-08-10 12:03:53 +0900

    컴포넌트 분리

---
+++ tic-tac-toe/src/tic-tac-toe/components/Squares/Squares.jsx
@@ -0,0 +1,12 @@
+import Square from '../Square/Square';
+import S from './Squares.module.css';
+
+function Squares() {
+  return (
+    <div className={S.component}>
+      <Square />
+    </div>
+  );
+}
+
+export default Squares;
---
+++ tic-tac-toe/src/tic-tac-toe/components/Status/Status.jsx
@@ -0,0 +1,7 @@
+import S from './Status.module.css';
+
+function Status() {
+  return <h2 className={S.component}>플레이어 : 🟨</h2>;
+}
+
+export default Status;
